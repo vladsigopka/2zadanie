6c32d09b76db668e95bc042be679ea4c0644399e
vladsigopka <vlad.tereshhenko.00@mail.ru>
2023-02-16 22:57:34 +0700

    добавление формы

--- js/main.js
+++ js/main.js
@@ -41,9 +41,56 @@ Vue.component('cols', {
             } else {
                 his.errors.push("Во второй колонке должно быть не более пяти карточек")
             }
+
+        })
+        eventBus.$on('addColumn3', card => {
+            this.column3.push(card)
+            this.column2.splice(this.column2.indexOf(card), 1)
+        })
+        eventBus.$on('addColumn1-3', card => {
+            this.column3.push(card)
+            this.column1.splice(this.column1.indexOf(card), 1)
         })
 
     }
+})
+
+Vue.component('newcard', {
+    template: `
+    <form class="addform" @submit.prevent="onSubmit">
+            <label for="title">Ведите заметку </label>
+        <p>
+            <input id="title" required v-model="title" maxlength="30" type="text" placeholder="название">
+        </p>
+        <div>
+            <input required id="subtask1" v-model="subtask1" maxlength="30" placeholder="задача">
+        </div>
+        <div>
+            <input required id="subtask2" v-model="subtask2" maxlength="30" placeholder="задача">
+        </div>
+        <div>
+            <input required id="subtask3" v-model="subtask3" maxlength="30" placeholder="задача">
+        </div>
+        <div>
+            <input  id="subtask4" v-model="subtask4" maxlength="30" placeholder="задача">
+        </div>
+        <div>
+            <input  id="subtask5" v-model="subtask5" maxlength="30" placeholder="задача">
+        </div>
+        <button type="submit">добавить заметку</button>
+    </form>
+    `,
+    data() {
+        return {
+            title: null,
+            subtask1: null,
+            subtask2: null,
+            subtask3: null,
+            subtask4: null,
+            subtask5: null,
+            errors: [],
+        }
+    },
 })
 
 let app = new Vue({
