9ba4cba28e317d49f7bee15cd412dd883f3385ad
vladsigopka <vlad.tereshhenko.00@mail.ru>
2023-02-16 22:50:59 +0700

    добавление нескольких колонок

--- js/main.js
+++ js/main.js
@@ -23,10 +23,31 @@ Vue.component('cols', {
             column3: [],
             errors: []
         }
-    },})
+    },
+    mounted() {
+        eventBus.$on('addColumn1', card => {
+            this.errors = []
+            if (this.column1.length < 3) {
+                this.column1.push(card)
+            } else {
+                this.errors.push('В первой колонке должно быть не более трех карточек')
+            }
+        })
+        eventBus.$on('addColumn2', card => {
+            this.errors = []
+            if (this.column2.length < 5) {
+                this.column2.push(card)
+                this.column1.splice(this.column1.indexOf(card), 1)
+            } else {
+                his.errors.push("Во второй колонке должно быть не более пяти карточек")
+            }
+        })
+
+    }
+})
 
 let app = new Vue({
     el: '#app',
-    
+
 
 })
