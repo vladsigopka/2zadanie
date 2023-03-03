bb1a094448405b0a94c437f7d74eff671b8d89cf
vladsigopka <vlad.tereshhenko.00@mail.ru>
2023-03-03 18:40:16 +0700

    добавление заметки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 let eventBus = new Vue()
 
+
+
+
+
+
 Vue.component('cols', {
     template:`
  
@@ -61,6 +66,60 @@ Vue.component('cols', {
 
     }
 })
+
+Vue.component('newcard', {
+    template: `
+    <form class="addform" @submit.prevent="onSubmit">
+            <label for="title">Ведите заметку </label>
+        <p>
+            <input id="title" required v-model="title" maxlength="30" type="text" placeholder="название">
+        </p>
+        <div>
+            <input required id="subtask1" v-model="subtask1" maxlength="30" placeholder="задача">
+        </div>
+        <div>
+            <input required id="subtask2" v-model="subtask2" maxlength="30" placeholder="задача">
+        </div>
+        <div>
+            <input required id="subtask3" v-model="subtask3" maxlength="30" placeholder="задача">
+        </div>
+        <div>
+            <input  id="subtask4" v-model="subtask4" maxlength="30" placeholder="задача">
+        </div>
+        <div>
+            <input  id="subtask5" v-model="subtask5" maxlength="30" placeholder="задача">
+        </div>
+        <button type="submit">добавить заметку</button>
+    </form>
+    `,
+    data() {
+        return {
+            title: null,
+            subtask1: null,
+            subtask2: null,
+            subtask3: null,
+            subtask4: null,
+            subtask5: null,
+            errors: [],
+        }
+    },
+    methods: {
+        onsubmit(){
+            let card = {
+                title: this.title,
+                subtasks: [{
+                    title: this.subtask1, completed: false},
+                    {title: this.subtask2, completed: false},
+                    {title: this.subtask3, completed: false},
+                    {title: this.subtask4, completed: false},
+                    {title: this.subtask5, completed: false}
+                ]
+            }
+        }
+}
+})
+
+
 Vue.component('col1' , {
     template: `
     <div class="col">
@@ -186,43 +245,7 @@ Vue.component('col3', {
     },
 })
 
-Vue.component('newcard', {
-    template: `
-    <form class="addform" @submit.prevent="onSubmit">
-            <label for="title">Ведите заметку </label>
-        <p>
-            <input id="title" required v-model="title" maxlength="30" type="text" placeholder="название">
-        </p>
-        <div>
-            <input required id="subtask1" v-model="subtask1" maxlength="30" placeholder="задача">
-        </div>
-        <div>
-            <input required id="subtask2" v-model="subtask2" maxlength="30" placeholder="задача">
-        </div>
-        <div>
-            <input required id="subtask3" v-model="subtask3" maxlength="30" placeholder="задача">
-        </div>
-        <div>
-            <input  id="subtask4" v-model="subtask4" maxlength="30" placeholder="задача">
-        </div>
-        <div>
-            <input  id="subtask5" v-model="subtask5" maxlength="30" placeholder="задача">
-        </div>
-        <button type="submit">добавить заметку</button>
-    </form>
-    `,
-    data() {
-        return {
-            title: null,
-            subtask1: null,
-            subtask2: null,
-            subtask3: null,
-            subtask4: null,
-            subtask5: null,
-            errors: [],
-        }
-    },
-})
+
 
 
 
